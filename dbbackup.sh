005fa128a2157c70fbf16ddc1efbc7a042af0642
mladjom <mladenmilentijevic@gmail.com>
2020-01-24 15:05:58 +0100

    Update

--- dbbackup.sh
+++ dbbackup.sh
@@ -9,5 +9,6 @@ for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "sys" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != "phpmyadmin" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
         sudo mysqldump -u $USER -p$PASSWORD --databases $db > /vagrant/database/$db.sql
+        echo -e "\e[96m Database successfully backed up \e[39m"
     fi
 done
